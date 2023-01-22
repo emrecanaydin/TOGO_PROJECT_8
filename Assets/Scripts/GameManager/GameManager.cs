5a6829a1ae921eca5ac7b4b03a49f3232e581c17
emrecanaydin <mrcnaydn@gmail.com>
2023-01-22 20:46:33 +0300

    updates..

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using PathCreation;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,13 +12,22 @@ public class GameManager : MonoBehaviour
     [Header("Player")]
     public Transform playerReference;
     public float playerMoveSpeed;
+    public GameObject playerTopCanvas;
+    public TextMeshProUGUI playerTopText;
+    public float selledBreadCount;
+
+    [Header("Taking")]
+    public Image takingFillImage;
 
     [Header("Path Creator")]
     public PathCreator pathCreator;
 
+    [Header("Panel")]
+    public TextMeshProUGUI profitText;
+
     [Header("Zone Refecences")]
-    public GameObject bakeryReference;
     public GameObject takingReference;
+    public GameObject bakedReference;
     public GameObject storeReference;
 
     [Header("Prefeabs")]
--- Assets/Scripts/Player/Collision.cs
+++ Assets/Scripts/Player/Collision.cs
@@ -1,78 +1,141 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
 
-//public class Collision : MonoBehaviour
-//{
-//    GameManager GM;
-//    bool canStackHamur;
+public class Collision : MonoBehaviour
+{
+    GameManager GM;
+    bool canCollect;
 
+    private void Start()
+    {
+        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
 
-//    private void Start()
-//    {
-//        GM = GameObject.FindGameObjectWithTag("GameManager").gameObject.GetComponent<GameManager>();
-//    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CollectCircle")
+        {
+            canCollect = true;
+            StartCoroutine(StartCollect());
+            GM.playerTopCanvas.SetActive(true);
+        }
+        if (other.tag == "TakeCircle")
+        {
+            StartCoroutine(StartTake());
+            GM.playerTopCanvas.SetActive(true);
+        }
+        if(other.tag == "BakeCircle")
+        {
+            StartCoroutine(StartBaking());
+            GM.playerTopCanvas.SetActive(true);
+        }
+        if (other.tag == "StoreCircle")
+        {
+            StartCoroutine(StartSelling());
+        }
+    }
 
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if(other.tag == "HamurCircle")
-//        {
-//            canStackHamur = true;
-//            StartCoroutine(GetBread());
-//            GM.playerTopCanvas.SetActive(true);
-//        }
-//        if(other.tag == "TakeCircle")
-//        {
-//            StartCoroutine(StartTake());
-//            GM.playerTopCanvas.SetActive(true);
-//        }
-//    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "CollectCircle")
+        {
+            GM.takingFillImage.fillAmount += 1f * Time.deltaTime;
+        }
+    }
 
-//    private void OnTriggerExit(Collider other)
-//    {
-//        if (other.tag == "HamurCircle")
-//        {
-//            canStackHamur = false;
-//            GM.playerTopCanvas.SetActive(false);
-//        }
-//        if (other.tag == "TakeCircle")
-//        {
-//            GM.playerTopCanvas.SetActive(false);
-//        }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "CollectCircle")
+        {
+            canCollect = false;
+            GM.playerTopCanvas.SetActive(false);
+        }
+        if (other.tag == "TakeCircle")
+        {
+            GM.playerTopCanvas.SetActive(false);
+        }
+        if (other.tag == "BakeCircle")
+        {
+            GM.playerTopCanvas.SetActive(false);
+        }
+    }
 
+    private IEnumerator StartCollect()
+    {
 
-//    }
+        yield return new WaitForSeconds(1f);
+        GameObject clonedBread = Instantiate(GM.bread, transform.position, Quaternion.identity);
+        Transform collectPoint = transform.Find("CollectPoint");
+        clonedBread.transform.parent = collectPoint;
+        clonedBread.transform.position = new Vector3(collectPoint.transform.position.x, collectPoint.transform.position.y + GM.collectedList.Count, collectPoint.transform.position.z);
+        clonedBread.transform.localRotation = Quaternion.Euler(new Vector3(-90, 0, 50));
+        GM.collectedList.Add(clonedBread);
+        string countText = GM.collectedList.Count == 5 ? "MAX \n" : "";
+        countText += $"{GM.collectedList.Count} / 5";
+        GM.playerTopText.text = countText;
+        if (canCollect && GM.collectedList.Count < 5)
+        {
+            StartCoroutine(StartCollect());
+        }
+    }
 
-//    private IEnumerator StartTake()
-//    {
-//        yield return new WaitForSeconds(1f);
-//        foreach (var collected in GM.collectedList)
-//        {
-//            collected.transform.DOMove(GM.firin.transform.position, 1f);
-//        }
-//    }
+    private IEnumerator StartTake()
+    {
+        yield return new WaitForSeconds(1f);
+        foreach (var collected in GM.collectedList)
+        {
+            collected.transform.parent = GM.takingReference.transform;
+            collected.transform.DOMove(GM.takingReference.transform.position, 1f);
+        }
+        StartCoroutine(StartCooking());
+    }
 
-//    private IEnumerator GetBread()
-//    {
-//        yield return new WaitForSeconds(1f);
-//        GameObject clonedBread = Instantiate(GM.breadPrefab, transform.position, Quaternion.identity);
-//        Transform collectPoint = transform.Find("CollectPoint");
-//        clonedBread.transform.parent = collectPoint;
-//        clonedBread.transform.position = new Vector3(collectPoint.transform.position.x, collectPoint.transform.position.y + GM.collectedList.Count, collectPoint.transform.position.z);
-//        GM.collectedList.Add(clonedBread);
-//        if(GM.collectedList.Count == 5)
-//        {
-//            GM.playerTopCount.text = $"MAX \n {GM.collectedList.Count} / 5";
-//        }
-//        else
-//        {
-//            GM.playerTopCount.text = $"{GM.collectedList.Count} / 5";
-//        }
-//        if (canStackHamur && GM.collectedList.Count < 5)
-//        {
-//            StartCoroutine(GetBread());
-//        }
-//    }
+    private IEnumerator StartCooking()
+    {
+        int index = 0;
+        foreach (var collected in GM.collectedList)
+        {
+            yield return new WaitForSeconds(1f);
+            Vector3 position = new Vector3(GM.bakedReference.transform.position.x, GM.bakedReference.transform.position.y + index, GM.bakedReference.transform.position.z);
+            collected.transform.position = position;
+            collected.transform.parent = GM.bakedReference.transform;
+            index = index + 1;
+        }
+    }
 
-//}
+    private IEnumerator StartBaking()
+    {
+        yield return new WaitForSeconds(1f);
+        int index = 0;
+        foreach (var collected in GM.collectedList)
+        {
+            Transform collectPoint = transform.Find("CollectPoint");
+            collected.transform.parent = collectPoint;
+            Vector3 position = new Vector3(collectPoint.transform.position.x, collectPoint.transform.position.y + index, collectPoint.transform.position.z);
+            collected.transform.DOMove(position, 1f);
+            index = index + 1;
+        }
+    }
+
+    private IEnumerator StartSelling()
+    {
+        yield return new WaitForSeconds(1f);
+        int index = 0;
+        foreach (var collected in GM.collectedList)
+        {
+            collected.transform.parent = GM.storeReference.transform;
+            Vector3 position = new Vector3(GM.storeReference.transform.position.x, GM.storeReference.transform.position.y, GM.storeReference.transform.position.z + index);
+            collected.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            collected.transform.DOMove(position, 1f);
+            index = index + 1;
+        }
+        GM.selledBreadCount += GM.collectedList.Count;
+        float profit = GM.selledBreadCount * 5;
+        GM.profitText.text = profit.ToString();
+        GM.playerTopText.text = "";
+        GM.collectedList.RemoveRange(0, GM.collectedList.Count);
+    }
+
+}
